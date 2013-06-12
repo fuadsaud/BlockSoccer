cb9012d55fd6b883b4b6e19ce22ef200675d0dff
Fuad Saud <fuadksd@gmail.com>
2013-06-12 01:16:36 -0300

    Add constructor to Point

--- Point.cpp
+++ Point.cpp
@@ -1,5 +1,12 @@
 #include "Point.h"
 
+Point::Point()
+{
+    Point::x = 0;
+    Point::y = 0;
+    Point::z = 0;
+}
+
 Point::Point(float x, float y, float z)
 {
     Point::x = x;
--- Point.h
+++ Point.h
@@ -9,6 +9,7 @@ public:
     float y;
     float z;
 
+Point();
     Point(float x, float y, float z);
 
     Point* operator+(const Point* p) const;
